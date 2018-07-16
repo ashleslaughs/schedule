7ab36c6fa2fb28273b2a719f435622815ca7259a
ashley <lujanashlet@gmail.com>
2018-07-16 10:21:44 -0600

    integreated font awexzsome and built a custom icon component:

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import Library from './library';
+import Icon from '../icon';
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
                 <label className ="library-course__title"> Problem Solving </label> 
-                {/* icon componsnet */}
+                {Icon('fas fa-check', 'library-course__icon')}
                 {/* arrow component  */}
                 {/* action button component  */}
                 <div className="library-course__description">
